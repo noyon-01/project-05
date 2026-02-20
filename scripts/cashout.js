abd9e5cd2acaf4fb984f8398d0d547d1b0cf6a5a
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-21 04:47:35 +0600

    tasting

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -1,38 +1,38 @@
-document.getElementById("cashout_btn").addEventListener("click", function () {
-  const cashoutNumber = getValueFormInput("cashout_Input_number");
-  if (cashoutNumber.length != 11) {
-    alert("Invalid Agent Number");
-    return;
-  }
-  const cashoutAmount = getValueFormInput("cashout_Input_amount");
-  const currentBalance = getBalance();
+// document.getElementById("cashout_btn").addEventListener("click", function () {
+//   const cashoutNumber = getValueFormInput("cashout_Input_number");
+//   if (cashoutNumber.length != 11) {
+//     alert("Invalid Agent Number");
+//     return;
+//   }
+//   const cashoutAmount = getValueFormInput("cashout_Input_amount");
+//   const currentBalance = getBalance();
 
-  const newBalance = currentBalance - Number(cashoutAmount);
-  if (newBalance < 0) {
-    alert("Invalid Amount");
-    return;
-  }
+//   const newBalance = currentBalance - Number(cashoutAmount);
+//   if (newBalance < 0) {
+//     alert("Invalid Amount");
+//     return;
+//   }
 
-  const cashoutPin = getValueFormInput("cashout_pin");
-  if (cashoutPin === "1234") {
-    alert("Cashout is Successfully Done.");
-    setBalance(newBalance);
+//   const cashoutPin = getValueFormInput("cashout_pin");
+//   if (cashoutPin === "1234") {
+//     alert("Cashout is Successfully Done.");
+//     setBalance(newBalance);
 
-    // !Transaction Section Starts Here! //
-    const history = document.getElementById("transaction_history_container");
-    const newHistory = document.createElement("div");
-    newHistory.innerHTML = `
-    <div id="transactions_card" class="p-5 bg-base-100 rounded-2xl shadow">
-                <h1 class="text-2xl font-bold mb-2">Cashout</h1>
-                <p>Cashout ${cashoutAmount} Taka Form Agent No: ${cashoutNumber}, on ${new Date()}</p>
-    </div>`;
-    history.appendChild(newHistory);
-    // !Transaction Section Ends Here! //
-  } else {
-    alert("Invalid Pin Number.");
-    return;
-  }
-});
+//     // !Transaction Section Starts Here! //
+//     const history = document.getElementById("transaction_history_container");
+//     const newHistory = document.createElement("div");
+//     newHistory.innerHTML = `
+//     <div id="transactions_card" class="p-5 bg-base-100 rounded-2xl shadow">
+//                 <h1 class="text-2xl font-bold mb-2">Cashout</h1>
+//                 <p>Cashout ${cashoutAmount} Taka Form Agent No: ${cashoutNumber}, on ${new Date()}</p>
+//     </div>`;
+//     history.appendChild(newHistory);
+//     // !Transaction Section Ends Here! //
+//   } else {
+//     alert("Invalid Pin Number.");
+//     return;
+//   }
+// });
 
 // !Many Line Code is Not Recommended so Also functional code in Above! //
 // document.getElementById("cashout_btn").addEventListener("click", function () {
--- scripts/machine.js
+++ scripts/machine.js
@@ -1,30 +1,30 @@
-function getValueFormInput(id) {
-  const input = document.getElementById(id);
-  const inputValue = input.value;
-  return inputValue;
-}
+// function getValueFormInput(id) {
+//   const input = document.getElementById(id);
+//   const inputValue = input.value;
+//   return inputValue;
+// }
 
-function getBalance() {
-  const balance = document.getElementById("balance");
-  const currentBalance = balance.innerText;
-  return Number(currentBalance);
-}
+// function getBalance() {
+//   const balance = document.getElementById("balance");
+//   const currentBalance = balance.innerText;
+//   return Number(currentBalance);
+// }
 
-function setBalance(value) {
-  const balance = document.getElementById("balance");
-  balance.innerText = value;
-}
+// function setBalance(value) {
+//   const balance = document.getElementById("balance");
+//   balance.innerText = value;
+// }
 
-function showOnly(id) {
-  const addMoney = document.getElementById("add_money");
-  addMoney.classList.add("hidden");
+// function showOnly(id) {
+//   const addMoney = document.getElementById("add_money");
+//   addMoney.classList.add("hidden");
 
-  const cashout = document.getElementById("cashout");
-  cashout.classList.add("hidden");
+//   const cashout = document.getElementById("cashout");
+//   cashout.classList.add("hidden");
 
-  const transactions = document.getElementById("transactions");
-  transactions.classList.add("hidden");
+//   const transactions = document.getElementById("transactions");
+//   transactions.classList.add("hidden");
 
-  const selected = document.getElementById(id);
-  selected.classList.remove("hidden");
-}
+//   const selected = document.getElementById(id);
+//   selected.classList.remove("hidden");
+// }
