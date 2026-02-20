d4a3dfead11050238b42006e127e324b93730771
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-20 15:20:59 +0600

    login page is complete and added in project 5

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+document.getElementById("login_btn").addEventListener("click", function () {
+  const loginInput = document.getElementById("login_Input");
+  const numberInput = loginInput.value;
+  console.log(numberInput);
+
+  const pinInput = document.getElementById("login_pin");
+  const loginPin = pinInput.value;
+  console.log(loginPin);
+
+  if (numberInput === "01585735520" && loginPin === "1234") {
+    alert("Login Success");
+    window.location.assign("/home.html");
+  } else {
+    alert("Login Failed");
+    return;
+  }
+});
