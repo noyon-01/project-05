9a1e8c507930e15fe3534c25176f1cf9616ddc9e
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-21 04:52:06 +0600

    tasting

--- scripts/login.js
+++ scripts/login.js
@@ -9,7 +9,7 @@ document.getElementById("login_btn").addEventListener("click", function () {
 
   if (numberInput === "01585735520" && loginPin === "1234") {
     alert("Login Success");
-    window.location.assign("/home.html");
+    window.location.assign("./home.html");
   } else {
     alert("Login Failed");
     return;
